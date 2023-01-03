ccdfb3d1e9dbb4a6e862de82b9dd613ca8583e93
Maciej <mkuchcik@gmail.com>
2023-01-03 17:44:29 +0100

    Holding repaires

--- Assets/Scripts/Interactables/Holding.cs
+++ Assets/Scripts/Interactables/Holding.cs
@@ -14,6 +14,13 @@ public class Holding : Interactable
     [Range(0.0f, 1.0f)]
     public float useScale = .8f;
 
+    Vector3 defScale;
+
+    private void Start()
+    {
+        defScale = transform.localScale;
+    }
+
     public override Result<object, string> Action(params object[] args)
     {
         if (playerObject == null)
@@ -28,10 +35,18 @@ public class Holding : Interactable
         {
             transform.parent = playerObject.transform.Find("PlayerCamera");
             GetComponent<Rigidbody>().useGravity = false;
-            GetComponent<Rigidbody>().detectCollisions = false;
             GetComponent<Rigidbody>().isKinematic = true;
+
             isBeingHold = true;
-            transform.localScale = new Vector3(useScale, useScale, useScale);
+            transform.localScale = defScale * useScale;
+            Physics.IgnoreCollision(playerObject.GetComponent<Collider>(), GetComponent<Collider>(), true);
+
+
+
+            Vector3 joeBidenPosition = playerObject.transform.Find("PlayerCamera").transform.position;
+            Vector3 covidDistance = playerObject.transform.Find("PlayerCamera").transform.forward * howFar;
+            Vector3 getDown = Vector3.down * howDown;
+            transform.position = joeBidenPosition + covidDistance + getDown;
         }
 
         return Result<object, string>.Ok(gameObject);
@@ -41,25 +56,33 @@ public class Holding : Interactable
     {
         if (playerObject != null)
         {
+            Vector3 afterSpeed = playerObject.GetComponent<Rigidbody>().velocity;
+            //Vector3 afterSpeed2 = playerObject.GetComponent<Rigidbody>().angularVelocity;
+
+            Physics.IgnoreCollision(playerObject.GetComponent<Collider>(), GetComponent<Collider>(), false);
             isBeingHold = false;
             transform.parent = null;
+
             GetComponent<Rigidbody>().useGravity = true;
             GetComponent<Rigidbody>().isKinematic = false;
-            GetComponent<Rigidbody>().detectCollisions = true;
+
             playerObject = null;
-            transform.localScale = Vector3.one;
+            transform.localScale = defScale;
+
+            GetComponent<Rigidbody>().velocity = afterSpeed;// +(afterSpeed2*10);
         }
     }
 
 
     void FixedUpdate()
     {
-        if (isBeingHold)
+/*        if (isBeingHold)
         {
             Vector3 joeBidenPosition = (playerObject.transform.Find("PlayerCamera").transform.position
                 + playerObject.transform.Find("PlayerCamera").transform.forward * howFar)+Vector3.down*howDown;
             transform.position = joeBidenPosition;
-        }
+            transform.rotation = new Quaternion(0,0,0,0);
+        }*/
     }
 
 
