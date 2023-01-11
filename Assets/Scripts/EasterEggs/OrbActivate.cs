2d1ae1e0bbc8a5517e2e16ebd973b4df03867942
Maciej <mkuchcik@gmail.com>
2023-01-11 23:32:09 +0100

    fix2

--- Assets/Scripts/EasterEggs/OrbActivate.cs
+++ Assets/Scripts/EasterEggs/OrbActivate.cs
@@ -11,26 +11,26 @@ public class OrbActivate : Interactable
 
     Light oswietlenie;
     Material material;
+    Animator animator;
     public override void Awake()
     {
         base.Awake();
         oswietlenie = GetComponentInChildren<Light>();
         material = transform.Find("OrbColor").GetComponent<Renderer>().material;
+        animator = GetComponent<Animator>();
     }
     public override Result<object, string> Action(params object[] args)
     {
         if (!wasEnabled)
         {
+            t = 0.0f;
             wasEnabled = true;
             ParentSekret.Activate();
 
-            // TODO aktywuj g³os i poni¿sze zrób animacjami
-            material.color = Color.yellow;
-            material.SetColor("_EmissionColor", Color.yellow);
-            oswietlenie.intensity = 3.0f;
-            oswietlenie.color = Color.white;
-            oswietlenie.range = 10.0f;
+            
 
+
+            animator.SetBool("set", true);
 
             return Result<object, string>.Ok(wasEnabled.ToString());
         }
@@ -39,4 +39,23 @@ public class OrbActivate : Interactable
             return Result<object,string>.Err(wasEnabled.ToString());
         }
     }
+
+    float t = 0.0f;
+
+    void Update()
+    {
+        if (wasEnabled && t<1)
+        {
+            Color lerpMat = Color.Lerp(material.color, Color.yellow, t);
+            Color lerpEmiss = Color.Lerp(material.GetColor("_EmissionColor"), Color.yellow, t);
+
+            material.SetColor("_EmissionColor", lerpEmiss);
+            material.color = lerpMat;
+            oswietlenie.color = lerpMat;
+
+
+            t += Time.deltaTime / 77;
+        }
+    }
+
 }
