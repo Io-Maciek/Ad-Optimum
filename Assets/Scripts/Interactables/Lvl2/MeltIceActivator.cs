93d1b478d1e0a820c1efe35c8ea585f1600c9cf8
Maciej <mkuchcik@gmail.com>
2023-01-19 22:23:02 +0100

    better melting

--- Assets/Scripts/Interactables/Lvl2/MeltIceActivator.cs
+++ Assets/Scripts/Interactables/Lvl2/MeltIceActivator.cs
@@ -3,19 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using Assets.Scripts.Models;
 
-public class MeltIceActivator : Activator
+public class MeltIceActivator : MonoBehaviour
 {
-    public bool WasActivated { get; private set; } = false;
 
-
-    private void OnTriggerEnter(Collider other)
+    public Vector3 czyZmiejszac = new Vector3(0, 0, 0);
+    private void OnTriggerStay(Collider other)
     {
-
-        if (!WasActivated && other.GetComponent<Pochodnia>().IamFuming)
+        var x = other.GetComponent<Pochodnia>();
+        if (x != null && x.IamFuming)
         {
-            WasActivated = true;
-            GetComponent<Animator>().SetBool("Melt", true);
-        }
+            if (GetComponent<Animator>().enabled)
+            {
+                GetComponent<Animator>().enabled = false;
+            }
 
+            transform.localScale = new Vector3(transform.localScale.x - (Mathf.Abs(czyZmiejszac.x) * Time.deltaTime),
+                                                transform.localScale.y - (Mathf.Abs(czyZmiejszac.y) * Time.deltaTime),
+                                                transform.localScale.z - (Mathf.Abs(czyZmiejszac.z) * Time.deltaTime));
+            if (transform.localScale.x <= 0.01f || transform.localScale.y <= 0.01f || transform.localScale.z <= 0.01f)
+            {
+                Destroy(gameObject);
+            }
+            transform.localPosition = new Vector3(transform.localPosition.x + (czyZmiejszac.z * Time.deltaTime),
+                                                    transform.localPosition.y + (czyZmiejszac.y * Time.deltaTime),
+                                                    transform.localPosition.z + (czyZmiejszac.x * Time.deltaTime));
+        }
     }
 }
