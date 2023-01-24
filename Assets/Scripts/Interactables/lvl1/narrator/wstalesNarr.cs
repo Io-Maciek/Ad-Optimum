faf343d1a68a8d36c8c2dd4d5097139290854d8d
Maciej <mkuchcik@gmail.com>
2023-01-24 20:58:50 +0100

    stop on start

--- Assets/Scripts/Interactables/lvl1/narrator/wstalesNarr.cs
+++ Assets/Scripts/Interactables/lvl1/narrator/wstalesNarr.cs
@@ -5,7 +5,21 @@ using Assets.Scripts.Models;
 
 public class wstalesNarr : NarratorVoiceController
 {
+    public GameObject MyszkaObj;
     bool czyOdtworzono = false;
+
+
+    public override void Start()
+    {
+        base.Start();
+        if (_planszaInfo.ProgressValue == 0)
+        {
+            FindObjectOfType<PauzerScript>().enabled = false;
+            var p = FindObjectOfType<Controller>();
+            p.mouseMovement.enabled = false;
+            p.movement.enabled = false;
+        }
+    }
     void Update()
     {
         if (!czyOdtworzono)
@@ -14,7 +28,18 @@ public class wstalesNarr : NarratorVoiceController
             {
                 Play();
                 czyOdtworzono = true;
+                StartCoroutine("wait");
             }
         }
+    }
+
+    IEnumerator wait()
+    {
+        yield return new WaitForSeconds(Narracja.length - 4.5f);
+        MyszkaObj.SetActive(true);
+        var p = FindObjectOfType<Controller>();
+        p.mouseMovement.enabled = true;
+        p.movement.enabled = true;
+        FindObjectOfType<PauzerScript>().enabled = true;
     }
 }
